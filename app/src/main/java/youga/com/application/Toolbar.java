24eab86eafdfab57e4eb21e5bef550329caf51fc
YougaKing <yougakingwu@gmail.com>
2019-01-03 20:15:00 +0800

    oo

--- app/src/main/java/youga/com/application/Toolbar.java
+++ app/src/main/java/youga/com/application/Toolbar.java
@@ -7,7 +7,6 @@ import android.support.v7.widget.AppCompatTextView;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.Gravity;
-import android.view.View;
 import android.view.ViewGroup;
 
 /**
